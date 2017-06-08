f073a6428d9d6fb3070fdd213ecd67390bf28d12
Gilbert Kennen <gilbert@firewatcher.org>
2017-06-08 15:34:38 -0700

    Update tests to use elm-test 4.1.0

--- tests/Main.elm
+++ tests/Main.elm
@@ -1,18 +1,21 @@
-port module Main exposing (..)
+module Main exposing (..)
 
-import Check exposing (quickCheck, Claim, claim, that, is, for)
-import Check.Producer exposing (int, tuple, rangeInt)
 import Data.Integer exposing (..)
-import Legacy.Check.Test as CheckTest
-import Legacy.ElmTest as ElmTest exposing (..)
+import Expect
+import Fuzz exposing (Fuzzer, conditional, int, tuple)
+import Test exposing (..)
 import Maybe exposing (Maybe)
-import Test.Runner.Node exposing (run, TestProgram)
-import Json.Encode exposing (Value)
 
 
-integer : Check.Producer.Producer Integer
+integer : Fuzzer Integer
 integer =
-    Check.Producer.map (\a -> fromInt a) int
+    int |> Fuzz.map fromInt
+
+
+nonZeroInteger : Fuzzer Integer
+nonZeroInteger =
+    conditional { retries = 16, fallback = (+) 1, condition = (/=) 0 } int
+        |> Fuzz.map fromInt
 
 
 addTests : Test
@@ -27,34 +30,37 @@ addTests =
         three =
             fromInt 3
     in
-        suite "Add testsuite"
-            [ equals (add one two) three
-            ]
+        test "Add testsuite" <|
+            \_ -> Expect.equal (add one two) three
 
 
-qcAdd : Claim
+qcAdd : Test
 qcAdd =
-    Check.suite "Quickcheck Add"
-        [ for (is (that (claim "Conmutative adding") (\( a, b ) -> add a b)) (\( a, b ) -> add b a)) (tuple ( integer, integer ))
+    describe "Quickcheck Add"
+        [ fuzz (tuple ( integer, integer )) "Commutative adding" <|
+            \( a, b ) -> Expect.equal (add a b) (add b a)
         ]
 
 
 negateTests : Test
 negateTests =
-    let
-        one =
-            fromInt 1
-    in
-        suite "Negate testsuite"
-            [ equals (Data.Integer.negate (fromInt 1)) (fromInt -1)
-            , equals (Data.Integer.negate (fromInt -1)) (fromInt 1)
-            ]
+    describe "negate testsuite"
+        [ test "-(1) = -1" <|
+            \_ -> Expect.equal (Data.Integer.negate (fromInt 1)) (fromInt -1)
+        , test "-(-1) = 1" <|
+            \_ -> Expect.equal (Data.Integer.negate (fromInt -1)) (fromInt 1)
+        ]
 
 
-qcNegate : Claim
+qcNegate : Test
 qcNegate =
-    Check.suite "Quickcheck Negate"
-        [ for (is (that (claim "Double negate is noop") (\a -> (Data.Integer.negate (Data.Integer.negate a)))) (\a -> a)) integer
+    describe "Quickcheck Negate"
+        [ fuzz integer "Double negate is noop" <|
+            \a ->
+                a
+                    |> Data.Integer.negate
+                    |> Data.Integer.negate
+                    |> Expect.equal a
         ]
 
 
@@ -70,15 +76,15 @@ subTests =
         three =
             fromInt 3
     in
-        suite "Sub testsuite"
-            [ equals (sub three two) one
-            ]
+        describe "Sub testsuite"
+            [ test "3 - 2 = 1" <| \_ -> Expect.equal (sub three two) one ]
 
 
-qcSub : Claim
+qcSub : Test
 qcSub =
-    Check.suite "Quickcheck Sub"
-        [ for (is (that (claim "Conmutative substract") (\( a, b ) -> sub a b)) (\( a, b ) -> (Data.Integer.negate (sub b a)))) (tuple ( integer, integer ))
+    describe "Quickcheck Sub"
+        [ fuzz (tuple ( integer, integer )) "Conmutative substract" <|
+            \( a, b ) -> Expect.equal (sub a b) (Data.Integer.negate (sub b a))
         ]
 
 
@@ -94,172 +100,164 @@ mulTests =
         three =
             fromInt 3
     in
-        suite "Mul testsuite"
-            [ equals (mul three two) six
-            , equals (mul three (Data.Integer.negate two)) (Data.Integer.negate six)
+        describe "Mul testsuite"
+            [ test "3 * 2 = 6" <| \_ -> Expect.equal (mul three two) six
+            , test "3 * -2 = -6" <| \_ -> Expect.equal (mul three (Data.Integer.negate two)) (Data.Integer.negate six)
             ]
 
 
-qcMul : Claim
+qcMul : Test
 qcMul =
-    Check.suite "Quickcheck Mul"
-        [ for (is (that (claim "Conmutative multiplication") (\( a, b ) -> mul a b)) (\( a, b ) -> mul b a)) (tuple ( integer, integer ))
+    describe "Quickcheck Mul"
+        [ fuzz (tuple ( integer, integer )) "Conmutative multiplication" <|
+            \( a, b ) -> Expect.equal (mul a b) (mul b a)
         ]
 
 
 divmodTests : Test
 divmodTests =
-    suite "divmod testsuite"
-        [ equals (divmod (fromInt 2000000001) (fromInt 2)) (Just ( fromInt 1000000000, fromInt 1 ))
-        , equals (divmod (fromInt 2000000002) (fromInt 2)) (Just ( fromInt 1000000001, fromInt 0 ))
-        , equals (divmod (fromInt 20) (fromInt 0)) Nothing
+    describe "divmod testsuite"
+        [ test "divmod 2000000001 2 = Just (1000000000, 1)" <|
+            \_ ->
+                Expect.equal
+                    (divmod (fromInt 2000000001) (fromInt 2))
+                    (Just ( fromInt 1000000000, fromInt 1 ))
+        , test "divmod 2000000002 2 = Just (1000000001, 0)" <|
+            \_ ->
+                Expect.equal
+                    (divmod (fromInt 2000000002) (fromInt 2))
+                    (Just ( fromInt 1000000001, fromInt 0 ))
+        , test "divmod 20 0 == Nothing" <|
+            \_ ->
+                Expect.equal
+                    (divmod (fromInt 20) (fromInt 0))
+                    Nothing
         ]
 
 
-qcDivMod : Claim
+qcDivMod : Test
 qcDivMod =
-    Check.suite "Quickcheck divmod"
-        [ for
-            (is
-                (that (claim "divmod definition")
-                    (\( a, b ) ->
-                        let
-                            ( c, r ) =
-                                unsafeDivmod a b
-                        in
-                            add (mul c b) r
-                    )
-                )
-                (\( a, b ) -> a)
-            )
-            (tuple ( integer, Check.Producer.filter (\x -> neq x zero) integer ))
+    describe "Quickcheck divmod"
+        [ fuzz (tuple ( integer, nonZeroInteger ))
+            "divmod definition"
+          <|
+            \( a, b ) ->
+                let
+                    ( c, r ) =
+                        unsafeDivmod a b
+                in
+                    add (mul c b) r
+                        |> Expect.equal a
         ]
 
 
-qcAbs : Claim
+qcAbs : Test
 qcAbs =
-    Check.suite "Quickcheck abs"
-        [ for (is (that (claim "abs is always positive") (\a -> gte (Data.Integer.abs a) (fromInt 0))) (\a -> True)) integer
-        , for
-            (is
-                (that (claim "abs definition")
-                    (\a ->
-                        if gte a (fromInt 0) then
-                            (eq (Data.Integer.abs a) a)
-                        else
-                            (eq (Data.Integer.abs a) (Data.Integer.negate a))
-                    )
-                )
-                (\a -> True)
-            )
-            integer
+    describe "Quickcheck abs"
+        [ fuzz integer "abs is always positive" <|
+            \a ->
+                Expect.true "Expected |x| >= 0" (gte (Data.Integer.abs a) (fromInt 0))
+        , fuzz integer "abs definition" <|
+            \a ->
+                if gte a (fromInt 0) then
+                    Expect.true "Expected |x| = x; x >= 0" (eq (Data.Integer.abs a) a)
+                else
+                    Expect.true "Expected |x| = -x; x < 0" (eq (Data.Integer.abs a) (Data.Integer.negate a))
         ]
 
 
-qcSign : Claim
+qcSign : Test
 qcSign =
-    Check.suite "Quickcheck sign"
-        [ for
-            (is
-                (that (claim "sign definition")
-                    (\a ->
-                        if gte a zero then
-                            Positive
-                        else
-                            Negative
-                    )
-                )
-                (\a -> sign a)
-            )
-            integer
+    describe "Quickcheck sign"
+        [ fuzz integer "sign definition" <|
+            \a ->
+                if gte a zero then
+                    Expect.equal (sign a) Positive
+                else
+                    Expect.equal (sign a) Negative
         ]
 
 
 toStringTests : Test
 toStringTests =
-    suite "toString testsuite"
-        [ equals (Data.Integer.toString (fromInt 3345)) "3345"
-        , equals (Data.Integer.toString (fromInt (-3345))) "-3345"
+    describe "toString testsuite"
+        [ test "toString 3345 = \"3345\"" <|
+            \_ -> Expect.equal (Data.Integer.toString (fromInt 3345)) "3345"
+        , test "toString -3345 = \"-3345\"" <|
+            \_ -> Expect.equal (Data.Integer.toString (fromInt (-3345))) "-3345"
         ]
 
 
 fromStringTests : Test
 fromStringTests =
-    suite "fromString testsuite"
-        [ equals (Just (fromInt 1)) (fromString "1")
-        , equals (Just (fromInt -1)) (fromString "-1")
-        , equals (fromString "-a") Nothing
-        , equals (fromString "1234567890") (Just (fromInt 1234567890))
-        , equals (fromString "+1234567890") (Just (fromInt 1234567890))
+    describe "fromString testsuite"
+        [ test "fromString \"1\" = Just 1" <| \_ -> Expect.equal (Just (fromInt 1)) (fromString "1")
+        , test "fromString \"-1\" = Just -1" <| \_ -> Expect.equal (Just (fromInt -1)) (fromString "-1")
+        , test "fromString \"a\" = Nothing" <| \_ -> Expect.equal (fromString "-a") Nothing
+        , test "fromString \"1234567890\" = Just 1234567890" <|
+            \_ -> Expect.equal (fromString "1234567890") (Just (fromInt 1234567890))
+        , test "fromString \"+1234567890\" = 1234567890" <|
+            \_ -> Expect.equal (fromString "+1234567890") (Just (fromInt 1234567890))
         ]
 
 
 minMaxTests : Test
 minMaxTests =
-    suite "min-max testsuite"
-        [ equals (Data.Integer.max (fromInt 1234567890) (fromInt 3)) (fromInt 1234567890)
-        , equals (Data.Integer.max (fromInt 1) (fromInt 3)) (fromInt 3)
-        , equals (Data.Integer.min (fromInt 1234567890) (fromInt 3)) (fromInt 3)
-        , equals (Data.Integer.min (fromInt 1) (fromInt 3)) (fromInt 1)
+    describe "min-max testsuite"
+        [ test "max 1234567890 3 = 1234567890" <|
+            \_ ->
+                Expect.equal (Data.Integer.max (fromInt 1234567890) (fromInt 3)) (fromInt 1234567890)
+        , test "max 1 3 = 3" <| \_ -> Expect.equal (Data.Integer.max (fromInt 1) (fromInt 3)) (fromInt 3)
+        , test "min 1234567890 3 = 3" <|
+            \_ -> Expect.equal (Data.Integer.min (fromInt 1234567890) (fromInt 3)) (fromInt 3)
+        , test "min 1 3 = 1" <| \_ -> Expect.equal (Data.Integer.min (fromInt 1) (fromInt 3)) (fromInt 1)
         ]
 
 
 max_digit_valueTests : Test
 max_digit_valueTests =
-    suite "max_digit_value testsuite"
-        [ defaultTest
-            (assertNotEqual (max_digit_value * max_digit_value) ((max_digit_value * max_digit_value) + 1))
+    describe "max_digit_value testsuite"
+        [ test "max_digit_value ^ 2 /= 1 + max_digit_value ^ 2" <|
+            \_ ->
+                Expect.notEqual (max_digit_value * max_digit_value) ((max_digit_value * max_digit_value) + 1)
         ]
 
 
 compareTests : Test
 compareTests =
-    suite "compare testsuite"
-        [ equals (Data.Integer.compare (fromInt 1234567890) (fromInt 3)) GT
-        , equals (Data.Integer.compare (fromInt 3) (fromInt 3)) EQ
-        , equals (Data.Integer.compare (fromInt 1) (fromInt 3)) LT
-        , equals (gt (fromInt 1234567890) (fromInt 3)) True
-        , equals (gt (fromInt 3) (fromInt 3)) False
-        , equals (gt (fromInt 1) (fromInt 3)) False
-        , equals (gte (fromInt 1234567890) (fromInt 3)) True
-        , equals (gte (fromInt 3) (fromInt 3)) True
-        , equals (gte (fromInt 1) (fromInt 3)) False
-        , equals (eq (fromInt 1234567890) (fromInt 3)) False
-        , equals (eq (fromInt 3) (fromInt 3)) True
-        , equals (lt (fromInt 1234567890) (fromInt 3)) False
-        , equals (lt (fromInt 3) (fromInt 3)) False
-        , equals (lt (fromInt 1) (fromInt 3)) True
-        , equals (lte (fromInt 1234567890) (fromInt 3)) False
-        , equals (lte (fromInt 3) (fromInt 3)) True
-        , equals (lte (fromInt 1) (fromInt 3)) True
+    describe "compare testsuite"
+        [ test "compare 1234567890 3 = GT" <|
+            \_ -> Expect.equal (Data.Integer.compare (fromInt 1234567890) (fromInt 3)) GT
+        , test "compare 3 3 = EQ" <|
+            \_ -> Expect.equal (Data.Integer.compare (fromInt 3) (fromInt 3)) EQ
+        , test "compare 1 3 = LT" <|
+            \_ -> Expect.equal (Data.Integer.compare (fromInt 1) (fromInt 3)) LT
+        , test "1234567890 > 3" <|
+            \_ -> Expect.equal (gt (fromInt 1234567890) (fromInt 3)) True
+        , test "not ( 3 > 3 )" <|
+            \_ -> Expect.equal (gt (fromInt 3) (fromInt 3)) False
+        , test "not ( 1 > 3 )" <|
+            \_ -> Expect.equal (gt (fromInt 1) (fromInt 3)) False
+        , test "1234567890 >= 3" <|
+            \_ -> Expect.equal (gte (fromInt 1234567890) (fromInt 3)) True
+        , test "3 >= 3" <|
+            \_ -> Expect.equal (gte (fromInt 3) (fromInt 3)) True
+        , test "not ( 1 >= 3 )" <|
+            \_ -> Expect.equal (gte (fromInt 1) (fromInt 3)) False
+        , test "not ( 1234567890 = 3 )" <|
+            \_ -> Expect.equal (eq (fromInt 1234567890) (fromInt 3)) False
+        , test "3 = 3" <|
+            \_ -> Expect.equal (eq (fromInt 3) (fromInt 3)) True
+        , test "not ( 1234567890 < 3 )" <|
+            \_ -> Expect.equal (lt (fromInt 1234567890) (fromInt 3)) False
+        , test "not ( 3 < 3 )" <|
+            \_ -> Expect.equal (lt (fromInt 3) (fromInt 3)) False
+        , test "1 < 3" <|
+            \_ -> Expect.equal (lt (fromInt 1) (fromInt 3)) True
+        , test "not ( 1234567890 <= 3 )" <|
+            \_ -> Expect.equal (lte (fromInt 1234567890) (fromInt 3)) False
+        , test "3 <= 3" <|
+            \_ -> Expect.equal (lte (fromInt 3) (fromInt 3)) True
+        , test "1 <= 3" <|
+            \_ -> Expect.equal (lte (fromInt 1) (fromInt 3)) True
         ]
-
-
-allTests : Test
-allTests =
-    suite "All tests"
-        [ addTests
-        , negateTests
-        , subTests
-        , mulTests
-        , toStringTests
-        , fromStringTests
-        , minMaxTests
-        , compareTests
-        , divmodTests
-        , max_digit_valueTests
-        , CheckTest.evidenceToTest (quickCheck qcAdd)
-        , CheckTest.evidenceToTest (quickCheck qcNegate)
-        , CheckTest.evidenceToTest (quickCheck qcSub)
-        , CheckTest.evidenceToTest (quickCheck qcMul)
-        , CheckTest.evidenceToTest (quickCheck qcDivMod)
-        , CheckTest.evidenceToTest (quickCheck qcAbs)
-        , CheckTest.evidenceToTest (quickCheck qcSign)
-        ]
-
-
-main =
-    run emit allTests
-
-
-port emit : ( String, Value ) -> Cmd msg
